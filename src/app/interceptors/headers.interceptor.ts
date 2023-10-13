ae0fb3095578e4e2fd02db9c01e5fd1e2fc4dc49
Maria Isabel <mariacifuentescc@gmail.com>
2023-10-13 10:46:09 -0500

    api

--- src/app/interceptors/headers.interceptor.ts
+++ src/app/interceptors/headers.interceptor.ts
@@ -13,12 +13,12 @@ export class HeadersInterceptor implements HttpInterceptor {
 
     // Clone the request and replace the original headers with
     // cloned headers, updated with the authorization.
-    // '98aa390a62msh3af6cc595ae2121p189d3fjsnc19aaeea9cb6' maria
-    //'867b235d0dmsh8f5fbaa45a9b047p1ad368jsne325fb7d8d75' tia dalma
+    // '98aa390a62msh3af6cc595ae2121p189d3fjsnc19aaeea9cb6' maria //api
+    //'867b235d0dmsh8f5fbaa45a9b047p1ad368jsne325fb7d8d75' tia dalma //api
     
     const authReq = req.clone({
         headers: req.headers
-        .append ( 'X-RapidAPI-Key','98aa390a62msh3af6cc595ae2121p189d3fjsnc19aaeea9cb6'  )
+        .append ( 'X-RapidAPI-Key','9867b235d0dmsh8f5fbaa45a9b047p1ad368jsne325fb7d8d75'  )
         .append ( 'X-RapidAPI-Host', 'one-piece-episodes.p.rapidapi.com' ),
         params: req.params.append('language', language)
     });
